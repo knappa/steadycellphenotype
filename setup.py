32ee6d219127a8812f973ac0b4895f9a357153f3
Adam Knapp <adam.knapp@instanton.org>
2021-04-13 10:17:21 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
         name='steady_cell_phenotype',
-        version='0.1.1',
+        version='0.1.2',
         author="A. C. Knapp",
         author_email="adam.knapp@medicine.ufl.edu",
         long_description=long_description,
